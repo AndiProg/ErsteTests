199e39ed3a55bdc6142e46e06e321375eb7f16ac
Andi <Andi@LAPTOP>
2018-03-23 23:50:10 +0100

    test

--- ErsteTests/HelloWorld.cpp
+++ ErsteTests/HelloWorld.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include <climits>
 int main(){
-	std::cout << "Hello World" << std::endl;
-	std::cout << "Hello World" << std::endl;
-
+	int i = 4;
+	i += 2;
+	std::cout << i << std::endl;
+	std::cout << INT_MAX << std::endl;
+	std::cout << sizeof(int) << std::endl;
+	std::cout << "Test" << std::endl;
 	system("pause");
 	return 0;
 }
